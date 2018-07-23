d8ad7a9493a35eb0328c57d911816823b7049580
static <705523636@qq.com>
2018-07-23 22:12:11 +0800

    spring-cloud-test

---
+++ microservice-consumer-movie/src/main/java/com/yl/microservice/MicroserviceConsumerMovieApplication.java
@@ -0,0 +1,14 @@
+package com.yl.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroserviceConsumerMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerMovieApplication.class, args);
+    }
+}
